e9d4884d6c42d63af5c348194c77c1b6b3998f68
Saitore973 <saitorevg@gmail.com>
2022-02-20 12:39:46 +0300

    create a class that generates new instances of users

---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    '''
+    A class that generate new instances of users
+    '''
+    user_list = []
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+    def save_user(self):
+
+        '''
+        A method that save an instance of a user in the user list
+        '''
+        User.user_list.append(self) 
