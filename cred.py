d6a2d8df4baf1c716972071f1dd685485ff2e163
Saitore973 <saitorevg@gmail.com>
2022-02-20 22:29:28 +0300

    add credentials class and methods

--- cred.py
+++ cred.py
@@ -1,3 +1,21 @@
+import random 
+import string
+
 class Credentials:
-    def __init__(self, password):
-        self.password = password 
+    '''
+    Class that generates new instances of credentials
+    '''
+    credentials_list = []
+
+    def __init__ (self,account_username, account_password):
+        '''
+        __init method that helps us define properties for our objects
+        '''
+        self.account_username = account_username
+        self.account_password = account_password
+
+    def delete_logins(self):
+        '''
+        method to delete credential object
+        '''
+        Credentials.credentials_list.remove(self)
