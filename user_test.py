ef2644451760237366bbdf37ea21ba36ccef7ee6
Saitore973 <saitorevg@gmail.com>
2022-02-21 12:40:48 +0300

    validate credentials

--- user_test.py
+++ user_test.py
@@ -1,26 +1,27 @@
-
 import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for the contact class behaviours.
+
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
     def SetUp(self):
-        self.new_account = User("saitore","Saitore@12")
-
+        self.new_account = User("saitore","Saitore@84")
+    
     def test_init(self):
-        self.assertEqual(self.new_account.user_name, "awadh")
-        self.assertEqual(self.new_account.password, "Dreezy@84")
-    def test_delete_user(self):
-        self.delete_user()
-        self.assertEqual(len(User.user_list), 0)    
-
+        self.assertEqual(self.new_account.user_name, "saitore")
+        self.assertEqual(self.new_account.password, "Saitore@84")
+    
     def test_save_user(self):
         self.save_user()
-        self.assertEqual(len(User.user_list),1) 
+        self.assertEqual(len(User.user_list),1)
+    def test_delete_user(self):
+        self.delete_user()
+        self.assertEqual(len(User.user_list), 0)
+
 if __name__ == '__main()':
-    unittest.main()       
+    unittest.main()
